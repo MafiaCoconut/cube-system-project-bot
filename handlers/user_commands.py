28edd3db5bc031d0007898f678a0aacf67376464
MafiaCoconut <alex.kach.2000@gmail.com>
2024-01-25 16:51:13 +0100

    работает /start и запоминается ФИО

--- handlers/user_commands.py
+++ handlers/user_commands.py
@@ -8,12 +8,18 @@ from aiogram.fsm.context import FSMContext
 from handlers import auxiliary
 from keyboards import inline
 
+import pandas as pd
+
 from config.log_def import set_func, set_func_and_person
 from utils.fluent import list_of_available_languages
+from utils.states import UserState
+from config.config import name, chat_id as chat_id_text, structural_division, \
+    question_1, question_2, question_3, question_4
 
 router = Router()
 tag = "user_commands"
 status = "debug"
+file_path = 'data/main.xlsx'
 
 
 @router.message(CommandStart())
@@ -21,12 +27,41 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
     function_name = "command_start_handler"
     set_func(function_name, tag, status)
 
-    await message.answer("Добро пожаловать!")
+    await message.answer("Добро пожаловать!\nВведите ваше ФИО через пробел.")
+
+    df = pd.read_excel(file_path)
+    print(df)
+    row_index = df.index[df['ID'] == message.chat.id].tolist()
+    if not row_index:
+
+        first_empty_cell = df["ID"].isna().idxmax() if df["ID"].isna().any() else len(df)
+
+        if first_empty_cell == len(df):
+            df.loc[first_empty_cell] = [str(message.chat.id)] + [pd.NA] * (len(df.columns) - 1)
+        else:
+            df.at[first_empty_cell, "ID"] = message.chat.id
+
+        df.to_excel(file_path, index=False)
+
+    await state.set_state(UserState.name)
 
 
-@router.message(Command("help"))
-async def command_help_handler(message: Message, state: FSMContext) -> None:
-    function_name = "main_menu_handler"
+@router.message(UserState.name)
+async def form_name_handler(message: Message, state: FSMContext) -> None:
+    function_name = "form_name_handler"
     set_func_and_person(function_name, tag, message)
 
-    await message.answer("Вывод help информации")
+    fio = message.text.split(' ')
+
+    if len(fio) != 3:
+        await message.answer("Неправильный формат. Повторите попытку.")
+    else:
+        df = pd.read_excel('data/main.xlsx')
+        row_index = df.index[df['ID'] == message.chat.id].tolist()[0]
+        df.at[row_index, name] = message.text
+        print(df)
+        df.to_excel(file_path, index=False)
+
+
+
+
--- utils/states.py
+++ utils/states.py
@@ -1,5 +1,14 @@
 from aiogram.fsm.state import StatesGroup, State
 
 
+class UserState(StatesGroup):
+    name = State()
+    structural_division = State()
+    question_1 = State()
+    question_2 = State()
+    question_3 = State()
+    question_4 = State()
+
+
 class ExampleState(StatesGroup):
     example_state = State()
